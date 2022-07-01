0ba4e94069a0d1acb8817771f64482a19bca21e3
“itsmeanubhav” <imanubhav074@gmail.com>
2022-07-01 10:21:15 +0530

    Added :- logic to calculate Temporary Employee Wage Calculation

--- util/UtilityFunctions.java
+++ util/UtilityFunctions.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class UtilityFunctions {
     static int perHourWage = 20;
     static int permEmpMaxOneDayEffort = 8;
+    static int partTimeEmployeeMaxOneDayEffort = 4;
     static int DailyAttendanceCheck;
     /*
     Use-Case 1 Logic Whether Employee Is Present Or Absent
@@ -24,13 +25,21 @@ public class UtilityFunctions {
         }
     }
     /*
-    Use-Case 2 Logic To Calculate The DailyWagesOf Employees
-    DailyWageCalc() method is being used to Calculate the daily Wage of an employee
+    Use-Case 3 Logic for checking whether employee is Part time Or Permanent
+    You Have to Enter P for permanent or T for Temporary
     */
     public static int dailyWageCalc(){
         int dailyWage = 0;
-
-        dailyWage = perHourWage * permEmpMaxOneDayEffort;
+        System.out.println("Please enter P is if the employee is Permanent Employee or T for Temporary Employee");
+        Scanner scan = new Scanner(System.in);
+        String tempOrPerm = scan.next();
+        if(tempOrPerm.equals("P")){ //Press P if the Employee is Permanent
+            dailyWage = perHourWage * permEmpMaxOneDayEffort;
+        }else if(tempOrPerm.equals("T")){   //Press T if the employee is Temporary
+            dailyWage = partTimeEmployeeMaxOneDayEffort * perHourWage;
+        }else{  //if any other input is provided
+            System.out.println("Please Provide only authorized input");
+        }
         return dailyWage;
     }
 }
